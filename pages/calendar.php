fb48e1c07619b88a119bc5da8d223126f9e6cefd
Hansanghyeon <999@hyeon.pro>
2020-12-22 18:05:28 +0900

    :sparkles: [PC] 캘린더 페이지 완성

---
+++ pages/calendar.php
@@ -0,0 +1,8 @@
+<?php include_once '../includes/head.php' ?>
+<link type="text/css" rel="stylesheet" href="../public/css/fullcalendar.css">
+
+<div class="calendarContainer">
+  <div class="innerWrap container">
+    <?php include_once './calendarSource.php' ?>
+  </div>
+</div>
