d6644f18b4c9cf696321ddf990670898ffc81337
Hansanghyeon <999@hyeon.pro>
2020-12-22 18:33:28 +0900

    :construction: 테스트환경 설정

--- pages/calendar.php
+++ pages/calendar.php
@@ -1,6 +1,4 @@
-<?php include_once '../includes/head.php' ?>
 <link type="text/css" rel="stylesheet" href="../public/css/fullcalendar.css">
-
 <div class="calendarContainer">
   <div class="innerWrap container">
     <?php include_once './calendarSource.php' ?>
