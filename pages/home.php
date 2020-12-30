0da294536e30de2f209eea721b2958162e256926
Hansanghyeon <999@hyeon.pro>
2020-12-30 15:21:37 +0900

    :construction: 입금 대기 및 여러가지 정보 섹션추가

---
+++ pages/home.php
@@ -0,0 +1,36 @@
+<?php include_once '../includes/head.php'; ?>
+<?php include_once '../includes/header.php'; ?>
+
+<main class="home-page">
+  <div class="section">
+    <div class="container">
+      <div class="date">2020.11.18 (수)</div>
+      <ul class="allInfo">
+        <li>
+          <div class="count">17</div>
+          <div class="title">입금대기</div>
+        </li>
+        <li>
+          <div class="count">3</div>
+          <div class="title">예약완료</div>
+        </li>
+        <li>
+          <div class="count">0</div>
+          <div class="title">예약취소</div>
+        </li>
+        <li>
+          <div class="count">3</div>
+          <div class="title">대기객실</div>
+        </li>
+        <li>
+          <div class="count">15</div>
+          <div class="title">사용중</div>
+        </li>
+        <li>
+          <div class="count">2</div>
+          <div class="title">청소중</div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</main>
