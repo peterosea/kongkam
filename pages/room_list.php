9eb85bbd0d4de84e846a4b916d0b1e0eb58a7d7e
Hansanghyeon <999@hyeon.pro>
2020-12-30 18:26:21 +0900

    :sparkles: 객실 관리 상세 element 추가완료

--- pages/room_list.php
+++ pages/room_list.php
@@ -451,6 +451,153 @@
         </div>
       </div>
     </div>
+    <div class="roomDetailView">
+      <div class="row">
+        <div class="col">
+          <div class="header">
+            <span>고객정보</span>
+          </div>
+          <div class="content">
+            <table class="customerInfo">
+              <tbody>
+                <tr>
+                  <th>고객명</th>
+                  <td>김정현 외 3명</td>
+                </tr>
+                <tr>
+                  <th>입실일</th>
+                  <td>2020. 11. 14</td>
+                </tr>
+                <tr>
+                  <th>퇴실일</th>
+                  <td>2020. 11.15</td>
+                </tr>
+                <tr>
+                  <th>전화번호</th>
+                  <td>010. 3245. 2411</td>
+                </tr>
+                <tr>
+                  <th>예약앱</th>
+                  <td>야놀자</td>
+                </tr>
+                <tr>
+                  <th>입금내역</th>
+                  <td>완료 (2020.10.30)</td>
+                </tr>
+                <tr>
+                  <th>메모</th>
+                  <td>
+                    반려견 2마리<br />
+                    바베큐그릴 준비
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="col">
+          <div class="header">
+            <span>객실상태관리</span>
+          </div>
+          <div class="content">
+            <table class="roomStateManagement">
+              <tbody>
+                <tr>
+                  <th>체크인</th>
+                  <td>김정현 외 3명</td>
+                </tr>
+                <tr>
+                  <th>재실여부</th>
+                  <td>2020.11.14</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="col">
+          <div class="header">
+            <span>냉난방관리</span>
+          </div>
+          <div class="content">
+            <ul class="roomOptionControl">
+              <li>
+                <form>
+                  <div class="roomOptionControlItemWrap">
+                    <div class="title">ROOM 1</div>
+                    <div class="switch">
+                      <label class="iosCheckbox">
+                        <input type="checkbox" />
+                        <i></i>
+                      </label>
+                    </div>
+                    <div class="temperature">
+                      <input type="number" hidden>
+                      <button class="decrement _crement"></button>
+                      <div class="valueWrap">
+                        <span class="value">25</span><span class="unit">°C</span>
+                      </div>
+                      <button class="increment _crement"></button>
+                    </div>
+                    <div class="submitWrap">
+                      <button type="submit" class="label _deposit">저장</button>
+                    </div>
+                  </div>
+                </form>
+              </li>
+              <li>
+                <form>
+                  <div class="roomOptionControlItemWrap">
+                    <div class="title">ROOM 2</div>
+                    <div class="switch">
+                      <label class="iosCheckbox">
+                        <input type="checkbox" />
+                        <i></i>
+                      </label>
+                    </div>
+                    <div class="temperature">
+                      <input type="number" hidden>
+                      <button class="decrement _crement"></button>
+                      <div class="valueWrap">
+                        <span class="value">25</span><span class="unit">°C</span>
+                      </div>
+                      <button class="increment _crement"></button>
+                    </div>
+                    <div class="submitWrap">
+                      <button type="submit" class="label _deposit">저장</button>
+                    </div>
+                  </div>
+                </form>
+              </li>
+              <li>
+                <form>
+                  <div class="roomOptionControlItemWrap">
+                    <div class="title">ROOM 2</div>
+                    <div class="switch">
+                      <label class="iosCheckbox">
+                        <input type="checkbox" />
+                        <i></i>
+                      </label>
+                    </div>
+                    <div class="temperature">
+                      <input type="number" hidden>
+                      <button class="decrement _crement"></button>
+                      <div class="valueWrap">
+                        <span class="value">25</span><span class="unit">°C</span>
+                      </div>
+                      <button class="increment _crement"></button>
+                    </div>
+                    <div class="submitWrap">
+                      <button type="submit" class="label _deposit">저장</button>
+                    </div>
+                  </div>
+                </form>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   </div>
 </main>
 
