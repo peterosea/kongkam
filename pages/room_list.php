e7154e7dee776db1219d5f3f48ba7de84463b9e2
999hyeon <dandan9509@gmail.com>
2021-01-05 12:37:14 +0900

    :sparkles: 객실관리 모바일 디테일 UI (#16)

--- pages/room_list.php
+++ pages/room_list.php
@@ -48,17 +48,8 @@
               </div>
             </div>
           </div>
-          <div class="inner_popup with_tail">
-            <div class="btn close"><i class="fas fa-times"></i></div>
-            <ul>
-              <li><label>고객명</label>김영희</li>
-              <li><label>인원</label>3명</li>
-              <li><label>숙박기간</label>2박 3일</li>
-              <li><label>입실</label>11월 12일</li>
-              <li><label>퇴실</label>11월 15일</li>
-              <li><label>예약앱</label>야놀자</li>
-              <li><label>메모</label>차량 여부 및 다른 사항들</li>
-            </ul>
+          <div class="inner_popup">
+            <?php include('roomDetailView.php') ?>
           </div>
         </div>
         <div class="room">
@@ -76,17 +67,8 @@
               </div>
             </div>
           </div>
-          <div class="inner_popup with_tail">
-            <div class="btn close"><i class="fas fa-times"></i></div>
-            <ul>
-              <li><label>고객명</label>김영희</li>
-              <li><label>인원</label>3명</li>
-              <li><label>숙박기간</label>2박 3일</li>
-              <li><label>입실</label>11월 12일</li>
-              <li><label>퇴실</label>11월 15일</li>
-              <li><label>예약앱</label>야놀자</li>
-              <li><label>메모</label>차량 여부 및 다른 사항들</li>
-            </ul>
+          <div class="inner_popup">
+            <?php include('roomDetailView.php') ?>
           </div>
         </div>
         <div class="room">
@@ -104,17 +86,8 @@
               </div>
             </div>
           </div>
-          <div class="inner_popup with_tail">
-            <div class="btn close"><i class="fas fa-times"></i></div>
-            <ul>
-              <li><label>고객명</label>김영희</li>
-              <li><label>인원</label>3명</li>
-              <li><label>숙박기간</label>2박 3일</li>
-              <li><label>입실</label>11월 12일</li>
-              <li><label>퇴실</label>11월 15일</li>
-              <li><label>예약앱</label>야놀자</li>
-              <li><label>메모</label>차량 여부 및 다른 사항들</li>
-            </ul>
+          <div class="inner_popup">
+            <?php include('roomDetailView.php') ?>
           </div>
         </div>
         <div class="room">
@@ -134,16 +107,8 @@
               </div>
             </div>
           </div>
-          <div class="inner_popup with_tail">
-            <ul>
-              <li><label>고객명</label>김영희</li>
-              <li><label>인원</label>3명</li>
-              <li><label>숙박기간</label>2박 3일</li>
-              <li><label>입실</label>11월 12일</li>
-              <li><label>퇴실</label>11월 15일</li>
-              <li><label>예약앱</label>야놀자</li>
-              <li><label>메모</label>차량 여부 및 다른 사항들</li>
-            </ul>
+          <div class="inner_popup">
+            <?php include('roomDetailView.php') ?>
           </div>
         </div>
         <div class="room">
@@ -161,16 +126,8 @@
               </div>
             </div>
           </div>
-          <div class="inner_popup with_tail">
-            <ul>
-              <li><label>고객명</label>김영희</li>
-              <li><label>인원</label>3명</li>
-              <li><label>숙박기간</label>2박 3일</li>
-              <li><label>입실</label>11월 12일</li>
-              <li><label>퇴실</label>11월 15일</li>
-              <li><label>예약앱</label>야놀자</li>
-              <li><label>메모</label>차량 여부 및 다른 사항들</li>
-            </ul>
+          <div class="inner_popup">
+            <?php include('roomDetailView.php') ?>
           </div>
         </div>
         <div class="room">
@@ -189,16 +146,8 @@
               </div>
             </div>
           </div>
-          <div class="inner_popup with_tail">
-            <ul>
-              <li><label>고객명</label>김영희</li>
-              <li><label>인원</label>3명</li>
-              <li><label>숙박기간</label>2박 3일</li>
-              <li><label>입실</label>11월 12일</li>
-              <li><label>퇴실</label>11월 15일</li>
-              <li><label>예약앱</label>야놀자</li>
-              <li><label>메모</label>차량 여부 및 다른 사항들</li>
-            </ul>
+          <div class="inner_popup">
+            <?php include('roomDetailView.php') ?>
           </div>
         </div>
         <div class="room">
@@ -216,16 +165,8 @@
               </div>
             </div>
           </div>
-          <div class="inner_popup with_tail">
-            <ul>
-              <li><label>고객명</label>김영희</li>
-              <li><label>인원</label>3명</li>
-              <li><label>숙박기간</label>2박 3일</li>
-              <li><label>입실</label>11월 12일</li>
-              <li><label>퇴실</label>11월 15일</li>
-              <li><label>예약앱</label>야놀자</li>
-              <li><label>메모</label>차량 여부 및 다른 사항들</li>
-            </ul>
+          <div class="inner_popup">
+            <?php include('roomDetailView.php') ?>
           </div>
         </div>
         <div class="room">
@@ -243,16 +184,8 @@
               </div>
             </div>
           </div>
-          <div class="inner_popup with_tail">
-            <ul>
-              <li><label>고객명</label>김영희</li>
-              <li><label>인원</label>3명</li>
-              <li><label>숙박기간</label>2박 3일</li>
-              <li><label>입실</label>11월 12일</li>
-              <li><label>퇴실</label>11월 15일</li>
-              <li><label>예약앱</label>야놀자</li>
-              <li><label>메모</label>차량 여부 및 다른 사항들</li>
-            </ul>
+          <div class="inner_popup">
+            <?php include('roomDetailView.php') ?>
           </div>
         </div>
         <div class="room">
@@ -272,16 +205,8 @@
               </div>
             </div>
           </div>
-          <div class="inner_popup with_tail">
-            <ul>
-              <li><label>고객명</label>김영희</li>
-              <li><label>인원</label>3명</li>
-              <li><label>숙박기간</label>2박 3일</li>
-              <li><label>입실</label>11월 12일</li>
-              <li><label>퇴실</label>11월 15일</li>
-              <li><label>예약앱</label>야놀자</li>
-              <li><label>메모</label>차량 여부 및 다른 사항들</li>
-            </ul>
+          <div class="inner_popup">
+            <?php include('roomDetailView.php') ?>
           </div>
         </div>
         <div class="room">
@@ -299,16 +224,8 @@
               </div>
             </div>
           </div>
-          <div class="inner_popup with_tail">
-            <ul>
-              <li><label>고객명</label>김영희</li>
-              <li><label>인원</label>3명</li>
-              <li><label>숙박기간</label>2박 3일</li>
-              <li><label>입실</label>11월 12일</li>
-              <li><label>퇴실</label>11월 15일</li>
-              <li><label>예약앱</label>야놀자</li>
-              <li><label>메모</label>차량 여부 및 다른 사항들</li>
-            </ul>
+          <div class="inner_popup">
+            <?php include('roomDetailView.php') ?>
           </div>
         </div>
         <div class="room">
@@ -327,16 +244,8 @@
               </div>
             </div>
           </div>
-          <div class="inner_popup with_tail">
-            <ul>
-              <li><label>고객명</label>김영희</li>
-              <li><label>인원</label>3명</li>
-              <li><label>숙박기간</label>2박 3일</li>
-              <li><label>입실</label>11월 12일</li>
-              <li><label>퇴실</label>11월 15일</li>
-              <li><label>예약앱</label>야놀자</li>
-              <li><label>메모</label>차량 여부 및 다른 사항들</li>
-            </ul>
+          <div class="inner_popup">
+            <?php include('roomDetailView.php') ?>
           </div>
         </div>
         <div class="room">
@@ -354,16 +263,8 @@
               </div>
             </div>
           </div>
-          <div class="inner_popup with_tail">
-            <ul>
-              <li><label>고객명</label>김영희</li>
-              <li><label>인원</label>3명</li>
-              <li><label>숙박기간</label>2박 3일</li>
-              <li><label>입실</label>11월 12일</li>
-              <li><label>퇴실</label>11월 15일</li>
-              <li><label>예약앱</label>야놀자</li>
-              <li><label>메모</label>차량 여부 및 다른 사항들</li>
-            </ul>
+          <div class="inner_popup">
+            <?php include('roomDetailView.php') ?>
           </div>
         </div>
         <div class="room">
@@ -381,16 +282,8 @@
               </div>
             </div>
           </div>
-          <div class="inner_popup with_tail">
-            <ul>
-              <li><label>고객명</label>김영희</li>
-              <li><label>인원</label>3명</li>
-              <li><label>숙박기간</label>2박 3일</li>
-              <li><label>입실</label>11월 12일</li>
-              <li><label>퇴실</label>11월 15일</li>
-              <li><label>예약앱</label>야놀자</li>
-              <li><label>메모</label>차량 여부 및 다른 사항들</li>
-            </ul>
+          <div class="inner_popup">
+            <?php include('roomDetailView.php') ?>
           </div>
         </div>
         <div class="room">
@@ -410,16 +303,8 @@
               </div>
             </div>
           </div>
-          <div class="inner_popup with_tail">
-            <ul>
-              <li><label>고객명</label>김영희</li>
-              <li><label>인원</label>3명</li>
-              <li><label>숙박기간</label>2박 3일</li>
-              <li><label>입실</label>11월 12일</li>
-              <li><label>퇴실</label>11월 15일</li>
-              <li><label>예약앱</label>야놀자</li>
-              <li><label>메모</label>차량 여부 및 다른 사항들</li>
-            </ul>
+          <div class="inner_popup">
+            <?php include('roomDetailView.php') ?>
           </div>
         </div>
         <div class="room">
@@ -437,165 +322,14 @@
               </div>
             </div>
           </div>
-          <div class="inner_popup with_tail">
-            <ul>
-              <li><label>고객명</label>김영희</li>
-              <li><label>인원</label>3명</li>
-              <li><label>숙박기간</label>2박 3일</li>
-              <li><label>입실</label>11월 12일</li>
-              <li><label>퇴실</label>11월 15일</li>
-              <li><label>예약앱</label>야놀자</li>
-              <li><label>메모</label>차량 여부 및 다른 사항들</li>
-            </ul>
+          <div class="inner_popup">
+            <?php include('roomDetailView.php') ?>
           </div>
         </div>
       </div>
     </div>
-    <div class="roomDetailView">
-      <div class="row">
-        <div class="col">
-          <div class="header">
-            <span>고객정보</span>
-          </div>
-          <div class="content">
-            <table class="customerInfo">
-              <tbody>
-                <tr>
-                  <th>고객명</th>
-                  <td>김정현 외 3명</td>
-                </tr>
-                <tr>
-                  <th>입실일</th>
-                  <td>2020. 11. 14</td>
-                </tr>
-                <tr>
-                  <th>퇴실일</th>
-                  <td>2020. 11.15</td>
-                </tr>
-                <tr>
-                  <th>전화번호</th>
-                  <td>010. 3245. 2411</td>
-                </tr>
-                <tr>
-                  <th>예약앱</th>
-                  <td>야놀자</td>
-                </tr>
-                <tr>
-                  <th>입금내역</th>
-                  <td>완료 (2020.10.30)</td>
-                </tr>
-                <tr>
-                  <th>메모</th>
-                  <td>
-                    반려견 2마리<br />
-                    바베큐그릴 준비
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-        <div class="col">
-          <div class="header">
-            <span>객실상태관리</span>
-          </div>
-          <div class="content">
-            <table class="roomStateManagement">
-              <tbody>
-                <tr>
-                  <th>체크인</th>
-                  <td>김정현 외 3명</td>
-                </tr>
-                <tr>
-                  <th>재실여부</th>
-                  <td>2020.11.14</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-        <div class="col">
-          <div class="header">
-            <span>냉난방관리</span>
-          </div>
-          <div class="content">
-            <ul class="roomOptionControl">
-              <li>
-                <form>
-                  <div class="roomOptionControlItemWrap">
-                    <div class="title">ROOM 1</div>
-                    <div class="switch">
-                      <label class="iosCheckbox">
-                        <input type="checkbox" />
-                        <i></i>
-                      </label>
-                    </div>
-                    <div class="temperature">
-                      <input type="number" hidden>
-                      <button class="decrement _crement"></button>
-                      <div class="valueWrap">
-                        <span class="value">25</span><span class="unit">°C</span>
-                      </div>
-                      <button class="increment _crement"></button>
-                    </div>
-                    <div class="submitWrap">
-                      <button type="submit" class="label _deposit">저장</button>
-                    </div>
-                  </div>
-                </form>
-              </li>
-              <li>
-                <form>
-                  <div class="roomOptionControlItemWrap">
-                    <div class="title">ROOM 2</div>
-                    <div class="switch">
-                      <label class="iosCheckbox">
-                        <input type="checkbox" />
-                        <i></i>
-                      </label>
-                    </div>
-                    <div class="temperature">
-                      <input type="number" hidden>
-                      <button class="decrement _crement"></button>
-                      <div class="valueWrap">
-                        <span class="value">25</span><span class="unit">°C</span>
-                      </div>
-                      <button class="increment _crement"></button>
-                    </div>
-                    <div class="submitWrap">
-                      <button type="submit" class="label _deposit">저장</button>
-                    </div>
-                  </div>
-                </form>
-              </li>
-              <li>
-                <form>
-                  <div class="roomOptionControlItemWrap">
-                    <div class="title">ROOM 2</div>
-                    <div class="switch">
-                      <label class="iosCheckbox">
-                        <input type="checkbox" />
-                        <i></i>
-                      </label>
-                    </div>
-                    <div class="temperature">
-                      <input type="number" hidden>
-                      <button class="decrement _crement"></button>
-                      <div class="valueWrap">
-                        <span class="value">25</span><span class="unit">°C</span>
-                      </div>
-                      <button class="increment _crement"></button>
-                    </div>
-                    <div class="submitWrap">
-                      <button type="submit" class="label _deposit">저장</button>
-                    </div>
-                  </div>
-                </form>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+    <div class="d-none d-md-block">
+      <?php include('roomDetailView.php') ?>
     </div>
   </div>
   </div>
@@ -603,17 +337,17 @@
 
 <script type="text/javascript">
   // 객실관리 리스트 팝업
-  const getRoom = document.querySelectorAll('.room');
+  const getRoom = document.querySelectorAll('.room .inner_wrap');
   const pop = '_pop';
   for (let i = 0; i < getRoom.length; i++) {
     getRoom[i].addEventListener('click', function(e) {
-      if (getRoom[i].classList.contains(pop)) {
-        getRoom[i].classList.remove(pop);
+      if (getRoom[i].parentElement.classList.contains(pop)) {
+        getRoom[i].parentElement.classList.remove(pop);
       } else {
         for (let j = 0; j < getRoom.length; j++) {
-          getRoom[j].classList.remove(pop);
+          getRoom[j].parentElement.classList.remove(pop);
         }
-        getRoom[i].classList.add(pop);
+        getRoom[i].parentElement.classList.add(pop);
       }
     });
   }
