5a82423b958969a09debaf1429997971314fa267
Hansanghyeon <999@hyeon.pro>
2021-01-20 12:28:12 +0900

    :sparkles: superadmin header, navtopbar 추가

---
+++ includes/header-superadmin.php
@@ -0,0 +1,30 @@
+<header class="superadmin pageHeader">
+  <div class="container">
+    <ul class="menu-list">
+      <li>
+        <a href="/pages/superadmin-page14.php">
+          <img src="../public/img/150.svg">
+          고객 관리
+        </a>
+      </li>
+      <li>
+        <a href="/pages/room_list.php">
+          <img src="../public/img/menuicon_01.svg">
+          객실 관리
+        </a>
+      </li>
+      <li>
+        <a href="/pages/calendar.php">
+          <img src="../public/img/menuicon_02.svg">
+          예약 관리
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <img src="../public/img/menuicon_03.svg">
+          정산 목록
+        </a>
+      </li>
+    </ul>
+  </div>
+</header>
--- includes/navtopbar-superadmin.php
+++ includes/navtopbar-superadmin.php
@@ -1,4 +1,8 @@
-<div class="top_nav_wrap">
+<!--
+  navtopbar-default와 다른점
+  classname + superadmin
+-->
+<div class="top_nav_wrap superadmin">
   <div class="logo">
     <a href="/pages/room_list.php">
       <img src="../public/img/33.png" srcset="../public/img/33@2x.png 2x, ../public/img/33@3x.png 3x">
@@ -6,7 +10,7 @@
   </div>
   <ul class="top_menu">
     <li class="user_name">
-      <a href="#" class="_name">김정현</a>님
+      <a href="#" class="_name"><img src="../public/img/133.svg" alt=""> <span>SuperAdmin</span></a>
     </li>
     <li class="logout">
       <a href="/">LOGOUT</a>
