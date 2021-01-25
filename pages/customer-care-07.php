ed985a764200aaa000a8a646b681c7e851c74d1a
Hansanghyeon <999@hyeon.pro>
2021-01-25 23:33:07 +0900

    :lipstick: mobile 고객관리 - popup

--- pages/customer-care-07.php
+++ pages/customer-care-07.php
@@ -27,33 +27,37 @@
         </div>
       </form>
     </div>
-    <div class="table">
-      <div class="row header">
-        <div class="col checkbox">선택</div>
-        <div class="col number">번호</div>
-        <div class="col managerId">주 관리자 ID</div>
-        <div class="col managerName">주 관리자 이름</div>
-        <div class="col buildingName">건물 이름</div>
-      </div>
-      <div class="content">
-        <?php
-        for ($i = 1; $i < 5; $i++) {
-          echo <<<HTML
-          <div class="row">
-            <div class="col checkbox">
-              <input type="checkbox">
-            </div>
-            <div class="col number">
-              $i
-            </div>
-            <div class="col managerId">Jlab0982</div>
-            <div class="col managerName">김정현</div>
-            <div class="col buildingName">유토피아 호텔</div>
-          </div>
-HTML;
-        }
-        ?>
-      </div>
+    <div class="tableWrap">
+      <table class="table" border="0" cellspacing="0" cellpadding="0">
+        <thead>
+          <tr class="row header">
+            <td class="col checkbox">선택</td>
+            <td class="col number">번호</td>
+            <td class="col managerId">주 관리자 ID</td>
+            <td class="col managerName">주 관리자 이름</td>
+            <td class="col buildingName">건물 이름</td>
+          </tr>
+        </thead>
+        <tbody class="content">
+          <?php
+          for ($i = 1; $i < 5; $i++) {
+            echo <<<HTML
+            <tr class="row">
+              <td class="col checkbox">
+                <input type="checkbox">
+              </td>
+              <td class="col number">
+                $i
+              </td>
+              <td class="col managerId">Jlab0982</td>
+              <td class="col managerName">김정현</td>
+              <td class="col buildingName">유토피아 호텔</td>
+            </tr>
+  HTML;
+          }
+          ?>
+        </tbody>
+      </table>
     </div>
     <div class="btnListItem">
       <button type="submit" class="label _deposit">저장</button>
